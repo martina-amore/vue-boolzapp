9827a183c975deef7f4c5cfef2cc53c468ce8457
Martina Amore <martina.amore02@gmail.com>
2020-11-23 16:06:27 +0100

    Cambio utente e schermate con messaggi ricevuti e inviati implementati

--- js/main.js
+++ js/main.js
@@ -26,40 +26,80 @@ const root = new Vue ({
         myUsername: {
             image: "img/avatar_io.jpg",
             name: "Nome Utente",
-            userMessage:"",
-            messages: []
+            userMessage:""
         },
         userList: [
         {
             image: "img/avatar_1.jpg",
             name: "Michele",
             lastAccess: "Ultimo accesso oggi alle",
-            messages: "pippo"
+            messages: [
+            {
+                sent: ["prova inviato Michele", "altra prova inviato"],
+                received: ["prova ricevuto Michele", "altra prova ricevuto"],
+                date: ""
+            }
+        ],
+            selected: true
         },
         {
             image: "img/avatar_2.jpg",
             name: "Fabio",
             lastAccess: "Ultimo accesso oggi alle",
-            messages: "formaggino"
+            messages: [
+                {
+                sent: ["prova inviato Fabio"],
+                received: ["prova ricevuto Fabio"],
+                date: ""
+                }
+        ],
+            selected: false
         },
         {
             image: "img/avatar_3.jpg",
             name: "Samuele",
             lastAccess: "Ultimo accesso oggi alle",
-            messages: "caio"
+            messages: [
+                {
+                sent: ["prova inviato Samuele"],
+                received: ["prova ricevuto Samuele"],
+                date: ""
+                }
+        ],
+            selected: false
         },
         {
             image: "img/avatar_4.jpg",
             name: "Alessio",
             lastAccess: "Ultimo accesso oggi alle",
-            messages: "sempronio"
+            messages: [
+                {
+                sent: ["prova inviato Alessio"],
+                received: ["prova ricevuto Alessio"],
+                date: ""
+                }
+        ],
+            selected: false
         }
-    ]
+    ],
+        currentIndex: 0,
     },
     methods: {
-        sendMessage: function(input){
-            this.myUsername.messages.push(input);
+        sendMessage: function(input, index){
+            // this.userList[index].messages[0].sent.push(input);
+            // this.myUsername.messages.push(input);
+        },
+        changeUserActive: function(index){
+            this.currentIndex = index;
+            this.userList[index].selected = true;
+            // document.getElementById("prova").classList.add("active");
+            // document.getElementById("prova").nextSibling.classList.remove("active");
+            // this.userList[index].selected = true;
+            for (var i = 0; i < this.userList.length; i++) {
+                if (this.userList[index].selected == false ) {
+                    this.currentIndex = index;
+                }
+            }
         }
-
     }
 })
